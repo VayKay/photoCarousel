863c0fe17911dc497b011315f6db95cdea214d08
cnsarmi <christiannsarmiento@gmail.com>
2019-07-27 11:56:46 -0700

    Completed seeding for postgres with optimization

--- server/db/unsplashHelper.js
+++ server/db/unsplashHelper.js
@@ -1,10 +1,10 @@
-
+const api = require('../../unsplasherAPI.js')
 const axios = require('axios');
 
 module.exports = {
   getImages: (query, cb) => {
     const url = `https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=30&orientation=landscape`;
-    axios.get(url, { headers: { Authorization: 'Client-ID (Insert your API key here)' } })
+    axios.get(url, { headers: { Authorization: `Client-ID ${api}` } })
       .then((response) => {
         cb(null, response.data);
       })
